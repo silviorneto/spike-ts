05818d23fb5c82b1492a89c887e1b438c934a470
Silvio Romano Neto <silvioromano.dev@gmail.com>
2023-03-20 12:56:47 -0300

    objects

--- app.ts
+++ app.ts
@@ -1,10 +1,5 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
-}
-
-const number1 = '5'
-const number2 = 2.8
-
-const result = add(number1, number2)
-
-console.log(result)
+var person = {
+    name: "Silvio",
+    age: 36
+};
+console.log(person.name);
