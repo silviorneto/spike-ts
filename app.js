b8feabe13df1d56bf4f24f900fe90f0714a07270
Silvio Romano Neto <silvioromano.dev@gmail.com>
2023-03-21 22:31:34 -0300

    return functions

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var favoriteActivity;
-favoriteActivity = "Sports";
-favoriteActivity = 1;
-favoriteActivity = null;
-var favoriteActivities;
-favoriteActivity = "Sports";
-favoriteActivity = 1;
-favoriteActivity = null;
+function combine3(input1, input2, resultType) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+var combineAges3 = combine3(20, 36, 'as-number');
+console.log("combineAges: ", combineAges3);
+var combineStringAges3 = combine3(20, 36, 'as-number');
+console.log("combineAges: ", combineStringAges3);
+var combineNames3 = combine3("Silvio", "Romano", 'as-text');
+console.log("combineNames: ", combineNames3);
---
+++ basic-types/returnFunctions.ts
@@ -0,0 +1,17 @@
+function add (n1: number, n2: number): number {
+    return n1 + n2
+}
+
+function printAdd (n1: number, n2: number): void {
+    console.log(n1 + n2) 
+}
+
+function addAndUndefined1 (n1: number, n2: number): undefined {
+    console.log(n1 + n2)
+    return 
+}
+
+function addAndUndefined2 (n1: number, n2: number): void {
+    console.log(n1 + n2)
+    return 
+}
